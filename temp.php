92903abb1292ed2fe72fb1df167a78c6ab2d0275
himanshu-temp <chodu.tujhe.1100@gmail.com>
2025-08-01 16:10:14 +0530

    Create temp.php

---
+++ temp.php
@@ -0,0 +1,7 @@
+#EXTM3U
+#EXT-X-VERSION:3
+#EXTINF:-1 tvg-id="willowtv" tvg-name="Willow TV" tvg-logo="https://mma.prnewswire.com/media/1919551/WillowTV_Logo.jpg?p=facebook" group-title="Sports",Willow TV
+#KODIPROP:inputstream=inputstream.adaptive
+#KODIPROP:inputstream.adaptive.license_type=org.w3.clearkey
+#KODIPROP:inputstream.adaptive.license_key={"keys":[{"kty":"oct","k":"f60e08a145804890492f315b61789ac5","kid":"5e833f4019554aa394ff6de2eb19bf78"}],"type":"temporary"}
+https://sundirectgo-live.pc.cdn.bitgravity.com/hd38/dth.mpd
